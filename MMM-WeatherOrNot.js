25f65d35c803f70bf796c2b20d4d648c6bff3c02
Mykle <25211804+mykle1@users.noreply.github.com>
2020-03-04 16:07:13 -0500

    module not refreshing fixed

--- MMM-WeatherOrNot.js
+++ MMM-WeatherOrNot.js
@@ -51,6 +51,10 @@ Module.register("MMM-WeatherOrNot",{
 
 	getDom: function() {
 
+
+	var getTimeStamp = new Date().getTime(); // to seed url so Dom/module refreshes
+
+
 		var iframe = document.createElement("IFRAME");
 		iframe.classList.add("iframe");
 		iframe.style = "border:none";
@@ -75,7 +79,7 @@ Module.register("MMM-WeatherOrNot",{
 		iframe.srcdoc = `<a class="weatherwidget-io" href="https://forecast7.com/${this.config.languages}/${this.config.locationCode}/${this.config.location}/${this.config.tempUnits}" data-label_1="${this.config.label}" data-label_2="${this.config.label2}" data-font="Roboto" data-icons="${this.config.icons}" data-days="${this.config.days}" data-theme="${this.config.theme}" data-basecolor="${this.config.bgColor}" data-textcolor="${this.config.textColor}" data-highcolor="${this.config.htColor}" data-lowcolor="${this.config.ltColor}" data-suncolor="${this.config.sunColor}" data-mooncolor="${this.config.moonColor}" data-cloudcolor="${this.config.cloudColor}" data-cloudfill="${this.config.cloudFill}" data-raincolor="${this.config.rainColor}" data-snowcolor="${this.config.snowColor}" ></a>
 		<script>
 		!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
-		</script>`+ new Date();
+		</script>`+ getTimeStamp;
 
 		return iframe;
 	},
